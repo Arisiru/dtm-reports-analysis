1c108e6384e43fc92d40022a5678e88d7fdba01c
Alan Spark <alan.spark@auto1.com>
2019-01-13 18:04:52 +0100

    added plots for return per topic(subplots) with pdf file storage

--- scripts/convertor_pdf2txt.py
+++ scripts/convertor_pdf2txt.py
@@ -1,4 +1,5 @@
-import PyPDF2 
+# this script requeres pdftotext installed, for mac install it with home brew poppler
+
 import os
 import multiprocessing
 
@@ -11,37 +12,24 @@ def convert(findex, ticker):
     if os.path.isdir(dir_ticker_pdf):
         for report_pdf in os.listdir(dir_ticker_pdf):
             if '.pdf' in report_pdf:
-                report_name, ext = report_pdf.split('.')
-                txt_report_path = os.path.join(dir_data_txt, findex, ticker, "%s.txt" % report_name)
-                if os.path.exists(txt_report_path):
-                    print("%s exists" % txt_report_path)
-                else:
-                    pdf_report_path = os.path.join(dir_ticker_pdf, report_pdf)
-                    print("work on %s" % pdf_report_path)
-                    with open(pdf_report_path,'rb') as pdf_file:
-                        read_pdf = PyPDF2.PdfFileReader(pdf_file)
-                        if read_pdf.isEncrypted:
-                            print("%s encripted" % txt_report_path)
-                            try:
-                               read_pdf.decrypt('')
-                            except:
-                                command = ("cp " + report_pdf + " temp.pdf; qpdf --password='' --decrypt temp.pdf " + report_pdf + "; rm temp.pdf")
-                                os.system(command)
-                                pdf_file = open(pdf_report_path,'rb')
-                                read_pdf = PyPDF2.PdfFileReader(pdf_file)
+                try:
+                    report_name, ext = report_pdf.split('.')
+                    txt_report_path = os.path.join(dir_data_txt, findex, ticker, "%s.txt" % report_name)
+                except:
+                    print("Bad file name %s" % report_pdf)
 
-                        number_of_pages = read_pdf.getNumPages()
-                        total = 0
-                        with open(txt_report_path, 'w') as text_file:
-                            for page_number in range(number_of_pages):
-                                try:
-                                    page = read_pdf.getPage(page_number)
-                                    page_content = page.extractText()
-                                    text_file.write(page_content)
-                                    total += len(page_content)
-                                except:
-                                    print("Can't read page %s of %s" % (page_number, pdf_report_path))    
-                        print("%s pages %s total chars %s" % (report_pdf, page_number, total))
+                if not os.path.exists(txt_report_path):
+                    pdf_report_path = os.path.join(dir_ticker_pdf, report_pdf)
+                    command = "pdftotext %s %s" % (pdf_report_path, txt_report_path)
+                    os.system(command)
+                    if os.path.exists(txt_report_path):
+                        result_file = open(txt_report_path, 'r')
+                        characters = sum(len(text_line) for text_line in result_file)
+                        print("%s converted, len=%s" % (pdf_report_path, characters))
+                    else:
+                        print("No file after conversion %s" % txt_report_path)                      
+                #else: 
+                    #print("%s exists" % txt_report_path)       
 
 for findx in os.listdir(dir_data_pdf):
     dir_findx = os.path.join(dir_data_pdf, findx)
