f0421d5ef9f42c5961016eabefe9428d58f833ed
Alan Spark <alan_spark@mckinsey.com>
2021-07-27 13:07:16 +0200

    Update on limits for weights

--- run_cpp_dtm.sh
+++ run_cpp_dtm.sh
@@ -1,13 +1,13 @@
 # Run the dynamic topic model.
 ../../cpp/dtm/dtm/main \
---ntopics=22 \
+--ntopics=20 \
 --mode=fit \
 --rng_seed=193748 \
 --initialize_lda=true \
---corpus_prefix=data_runs/run_32_22/run_32_22 \
---outname=data_runs/run_32_22/results \
+--corpus_prefix=data_runs/run_32_20/run_32_20 \
+--outname=data_runs/run_32_20/results \
 --top_chain_var=0.005 \
---alpha=0.01 \
+--alpha=0.0109532 \
 --lda_sequence_min_iter=10 \
 --lda_sequence_max_iter=50 \
 --lda_max_em_iter=50
