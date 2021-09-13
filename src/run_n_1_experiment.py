03bf9f8d65af3bdb967dc5bbdc7914bcfab70b9e
Alan Spark <alan_spark@mckinsey.com>
2021-09-13 21:33:53 +0200

    add: move run - 1 experiment to src folder

--- src/run_n_1_experiment.py
+++ src/run_n_1_experiment.py
@@ -46,8 +46,7 @@ def update_processed_tickers(ticker, tickers):
 
 def update_errors_tickers(ticker):
     with open(FILES['errors tickers'], 'a') as f:
-        for processed_ticker in sorted(tickers):
-            f.write('%s\n' % processed_ticker)
+        f.write('%s\n' % ticker)
 
     return None
 
@@ -59,33 +58,35 @@ def construct_dtm_run_without_ticker(ticker):
     return data
 
 
-def run_dtm(ticker, n=22, init_lda=False, seed=193748, prefix='19_22'):
+def run_dtm(ticker, n=22, init_lda=False, seed=193748, prefix='19_22', iter_lda_min=1, iter_lda_max=1, iter_lda_em_max=1):
+    run_code = 'run_%s-%s' % (prefix, ticker)
     command = '%s \
         --ntopics=%s \
         --mode=fit \
         --rng_seed=%s \
         --initialize_lda=%s \
-        --corpus_prefix=data_runs/run_%s-%s/run_%s-%s \
-        --outname=data_runs/run_%s-%s/results \
+        --corpus_prefix=%s \
+        --outname=%s \
         --top_chain_var=0.015 \
         --alpha=0.01 \
-        --lda_sequence_min_iter=5 \
-        --lda_sequence_max_iter=50 \
-        --lda_max_em_iter=50' % (
+        --lda_sequence_min_iter=%s \
+        --lda_sequence_max_iter=%s \
+        --lda_max_em_iter=%s' % (
         FILES['dtm command'],
         n,
         seed,
         'true' if init_lda else 'false',
-        prefix, ticker,
-        prefix, ticker,
-        prefix, ticker
+        os.path.join(DIRS['experiment'], run_code, run_code),
+        os.path.join(DIRS['experiment'], run_code, 'results'),
+        iter_lda_min,
+        iter_lda_max,
+        iter_lda_em_max
     )
-    # execute command and wait for it most likely something os. like
 
     return os.system(command)
 
 
-tickers = ['test1', 'test2', 'test3']  # get_tickers()
+tickers = get_tickers()
 processed_tickers = get_processed_tickers()
 
 for ticker in tickers:
@@ -96,7 +97,8 @@ for ticker in tickers:
     if run_dtm(ticker) == SUCCESS_CODE:
         processed_tickers = update_processed_tickers(ticker, processed_tickers)
     else:
-        print('Return form run %s' %)
+        print('Return form run %s' % ticker)
+        update_errors_tickers(ticker)
 
 
 # EOF
